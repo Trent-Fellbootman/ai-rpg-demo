19d586ee1cf46e157c615c0f529d8510ddb4380c
Trent Fellbootman <78004741+Trent-Fellbootman@users.noreply.github.com>
2024-09-09 14:23:22 +0800

    more styling to game UI and remove bottom bar in default layout

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Textarea } from "@nextui-org/input";
+import { Spacer } from "@nextui-org/spacer";
 
 export default function Page() {
   return (
     <div>
       <Image
         alt="Picture of the author"
+        className="rounded-xl"
         height={672}
         sizes="100vw"
         src="/sample-ai-image.jpg"
@@ -15,6 +17,7 @@ export default function Page() {
         }}
         width={1008}
       />
+      <Spacer y={2} />
       <Textarea
         label="Action"
         placeholder="Describe what you would like to do"
