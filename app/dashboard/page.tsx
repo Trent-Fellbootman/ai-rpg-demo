5260237e6d5206cd6742372936f910db38303df9
Trent Fellbootman <78004741+Trent-Fellbootman@users.noreply.github.com>
2024-09-10 14:15:14 +0800

    Add new session button on dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,17 @@
+import { Button } from "@nextui-org/button";
+import { Link } from "@nextui-org/link";
+
 import GameCardsView from "@/app/ui/dashboard/game-cards-view";
 
 export default function DashboardPage() {
-  return <GameCardsView />;
+  return (
+    <div>
+      <div className="flex justify-end">
+        <Button as={Link} color="primary" href="/game/new">
+          New Session
+        </Button>
+      </div>
+      <GameCardsView />
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,9 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <Providers
+          themeProps={{ attribute: "class", defaultTheme: "dark", children }}
+        >
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
