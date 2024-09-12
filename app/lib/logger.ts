d3bb9790846129f23aeb6857e8e8f4d64122f7ef
Trent Fellbootman <78004741+Trent-Fellbootman@users.noreply.github.com>
2024-09-12 20:29:04 +0800

    Change logger config

--- app/lib/logger.ts
+++ app/lib/logger.ts
@@ -1,19 +1,16 @@
 import pino, { Logger } from "pino";
-import PinoPretty from "pino-pretty";
-
-const stream = PinoPretty({
-  levelFirst: true,
-  colorize: true,
-});
 
 export const logger: Logger =
-  // process.env["NODE_ENV"] === "production"
-  //   ? // JSON in production
-  //     pino({ level: "warn" })
-  //   : // Pretty print in development
-  pino(
-    {
-      level: "debug",
-    },
-    stream,
-  );
+  process.env["DEPLOYMENT_MODE"] === "development"
+    ? // Pretty print in development
+      pino({
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+          },
+        },
+        level: "debug",
+      })
+    : // JSON in production
+      pino({ level: "debug" });
