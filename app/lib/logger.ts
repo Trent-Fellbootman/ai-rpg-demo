261e4fde69c8c1f036f693e5d9d27ccacd25eece
Trent Fellbootman <78004741+Trent-Fellbootman@users.noreply.github.com>
2024-09-12 20:12:04 +0800

    make production environment to use debugging logger as well

--- app/lib/logger.ts
+++ app/lib/logger.ts
@@ -1,10 +1,10 @@
 import pino, { Logger } from "pino";
 
 export const logger: Logger =
-  process.env["NODE_ENV"] === "production"
-    ? // JSON in production
-      pino({ level: "warn" })
-    : // Pretty print in development
+  // process.env["NODE_ENV"] === "production"
+  //   ? // JSON in production
+  //     pino({ level: "warn" })
+  //   : // Pretty print in development
       pino({
         transport: {
           target: "pino-pretty",
