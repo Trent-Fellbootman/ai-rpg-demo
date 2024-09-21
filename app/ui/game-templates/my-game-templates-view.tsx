47a2a199b93ce4f8c431097e5778a8e1c0d0bb05
Trent Fellbootman <78004741+Trent-Fellbootman@users.noreply.github.com>
2024-09-21 17:00:39 +0800

    Fix error in my-game-templates-view not showing child session count

--- app/ui/game-templates/my-game-templates-view.tsx
+++ app/ui/game-templates/my-game-templates-view.tsx
@@ -1,11 +1,18 @@
 import GameTemplateCardsView from "@/app/ui/game-templates/game-template-cards-view";
 import { getCurrentUser } from "@/app/lib/database-actions/user-actions";
-import { getGameTemplatesByUser } from "@/app/lib/database-actions/game-template-actions";
+import {
+  getGameTemplatesByUser,
+  getGameTemplateStatistics,
+} from "@/app/lib/database-actions/game-template-actions";
 
 export async function MyGameTemplatesView() {
   const userId = (await getCurrentUser()).id;
 
   const templates = await getGameTemplatesByUser(userId);
+  // TODO: optimize
+  const statistics = await Promise.all(
+    templates.map((template) => getGameTemplateStatistics(template.id)),
+  );
 
   if (templates.length === 0) {
     return (
@@ -15,5 +22,16 @@ export async function MyGameTemplatesView() {
     );
   }
 
-  return <GameTemplateCardsView gameTemplatesMetadata={templates} />;
+  return (
+    <GameTemplateCardsView
+      gameTemplatesMetadata={templates.map((template) => {
+        return {
+          ...template,
+          childSessionCount: statistics.filter(
+            (stat) => stat.id === template.id,
+          )[0].childSessionsCount,
+        };
+      })}
+    />
+  );
 }
